ef43beb16dbe7a05ea593a7ccc4dd302563a8d22
TrungNDHE182037 <169044473+TrungNDHE182037@users.noreply.github.com>
2024-05-19 22:53:44 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import AddSaler from './webpages/AddSaler';
 function App() {
   return (
     <div>
+      {/* npm install react-router-dom neu bi loi */}
       <BrowserRouter>
       <Routes>
         <Route index element={<HomePage/>}/>
