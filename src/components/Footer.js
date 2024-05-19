ecf40b838a922e552ced30cf4a0d1449db2db94c
DucLAHE182040 <duclahe182040@fpt.edu.vn>
2024-05-19 22:52:07 +0700

    19/5: Hoan thien Frontend Home page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,63 @@
 import React from 'react'
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import logo from '../utility/testlogo.png';
+import { FaFacebook } from "react-icons/fa";
+import { FaYoutube } from "react-icons/fa";
+import { AiFillTikTok } from "react-icons/ai";
+import { FaGithub } from "react-icons/fa";
+
+
+const ColStyle = {
+  marginTop: '5vh'
+}
+
+const TextStyle = {
+  marginTop: '2vh',
+  color: 'orange',
+}
+
+const IconListStyle = {
+  marginTop:'2vh',
+  color:'orange',
+  display:'flex',
+  gap:'1vw',
+  fontSize:'300%'
+}
 
 function footer() {
   return (
-    <div>footer</div>
+    <div style={{ backgroundColor: '#888', height: '30vh' }}>
+      <Container>
+        <Row>
+          <Col>
+            <img src={logo} style={{ height: '30vh' }} alt='logo' />
+          </Col>
+          <Col style={ColStyle}>
+            <h3>Tìm chúng tôi trên:</h3>
+            <div className='IconList' style={IconListStyle}>
+              <FaFacebook />
+              <FaYoutube />
+              <AiFillTikTok />
+              <FaGithub />
+            </div>
+          </Col>
+          <Col style={ColStyle}>
+            <h3>Hoặc liên hệ qua</h3>
+            <h5 style={TextStyle}>Email: Example@example.com</h5>
+            <h5 style={TextStyle}>Phone: 0912.345.678</h5>
+            <h5 style={TextStyle}>Địa chỉ: Hòa Lạc, Thạch Thất, Hà Nội</h5>
+          </Col>
+          <Col style={ColStyle}>
+            <h3>Điều hướng nhanh</h3>
+            <h5 style={TextStyle}><a href='#home' style={{ textDecoration: 'none', color: 'orange' }}>Trang chủ</a></h5>
+            <h5 style={TextStyle}><a href='#home' style={{ textDecoration: 'none', color: 'orange' }}>Tất cả sản phẩm</a></h5>
+            <h5 style={TextStyle}><a href='#home' style={{ textDecoration: 'none', color: 'orange' }}>Hỗ trợ</a></h5>
+          </Col>
+        </Row>
+      </Container>
+    </div>
   )
 }
 
--- src/components/HomeItemList.js
+++ src/components/HomeItemList.js
@@ -1,8 +1,257 @@
 import React from 'react'
+import Carousel from 'react-bootstrap/Carousel';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import logo from '../utility/testlogo.png';
+import { FaStarHalfAlt } from "react-icons/fa";
+import { FaStar } from "react-icons/fa";
+
+
+const ListItem = {
+  margin: "auto",
+  width: '80vw',
+  display: 'flex',
+  justifyContent: 'space-around',
+  alignItems: 'center',
+}
+const CardStyle = {
+  width: '20vw',
+  backgroundColor: 'white',
+  color: 'black',
+  border: 'solid 2px black'
+}
+
+const TitleStyle = {
+  textAlign: 'center',
+  paddingBottom: '5vh',
+  fontSize: 'x-large'
+}
+
+const MakeCenter = {
+  marginTop: '10px',
+  display: 'block',
+  margin: 'auto'
+}
+
+const StarStyle = {
+  display: 'flex',
+  margin: "10px 0",
+  fontSize: 'x-large',
+  color: 'orange'
+}
 
 function HomeItemList() {
   return (
-    <div>HomeItemList</div>
+    <div style={{ padding: '50px 0' }}>
+      <div className='title' style={TitleStyle}>
+        <h1>Sản phẩm mới nhất</h1>
+        <p>Những mặt hàng mới lên kệ vô cùng hấp dẫn</p>
+      </div>
+      <Carousel data-bs-theme="dark" style={{ paddingBottom: '7.5vh' }}>
+        <Carousel.Item>
+          <div style={ListItem}>
+            <Card style={CardStyle}>
+              <Card.Img variant="top" src={logo} />
+              <hr />
+              <Card.Body>
+                <Card.Title style={{ textAlign: 'center', fontSize: 'xx-large' }}>Sản phẩm A</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up the
+                  bulk of the card's content.
+                </Card.Text>
+                <a href='#home'>Tên người bán</a>
+                <div style={StarStyle}>
+                  <FaStar />
+                  <FaStar />
+                  <FaStar />
+                  <FaStar />
+                  <FaStarHalfAlt />
+                </div>
+                <h3 style={{ color: 'orange' }}>100.000đ</h3>
+                <Button variant="primary" style={MakeCenter}>Thêm vào giỏ hàng</Button>
+              </Card.Body>
+            </Card>
+            <Card style={CardStyle}>
+              <Card.Img variant="top" src={logo} />
+              <hr />
+              <Card.Body>
+                <Card.Title style={{ textAlign: 'center', fontSize: 'xx-large' }}>Sản phẩm A</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up the
+                  bulk of the card's content.
+                </Card.Text>
+                <a href='#home'>Tên người bán</a>
+                <div style={StarStyle}>
+                  <FaStar />
+                  <FaStar />
+                  <FaStar />
+                  <FaStar />
+                  <FaStarHalfAlt />
+                </div>
+                <h3 style={{ color: 'orange' }}>100.000đ</h3>
+                <Button variant="primary" style={MakeCenter}>Thêm vào giỏ hàng</Button>
+              </Card.Body>
+            </Card>
+            <Card style={CardStyle}>
+              <Card.Img variant="top" src={logo} />
+              <hr />
+              <Card.Body>
+                <Card.Title style={{ textAlign: 'center', fontSize: 'xx-large' }}>Sản phẩm A</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up the
+                  bulk of the card's content.
+                </Card.Text>
+                <a href='#home'>Tên người bán</a>
+                <div style={StarStyle}>
+                  <FaStar />
+                  <FaStar />
+                  <FaStar />
+                  <FaStar />
+                  <FaStarHalfAlt />
+                </div>
+                <h3 style={{ color: 'orange' }}>100.000đ</h3>
+                <Button variant="primary" style={MakeCenter}>Thêm vào giỏ hàng</Button>
+              </Card.Body>
+            </Card>
+          </div>
+        </Carousel.Item>
+        <Carousel.Item>
+          <div style={ListItem}>
+            <Card style={CardStyle}>
+              <Card.Img variant="top" src={logo} />
+              <hr />
+              <Card.Body>
+                <Card.Title style={{ textAlign: 'center', fontSize: 'xx-large' }}>Sản phẩm A</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up the
+                  bulk of the card's content.
+                </Card.Text>
+                <a href='#home'>Tên người bán</a>
+                <div style={StarStyle}>
+                  <FaStar />
+                  <FaStar />
+                  <FaStar />
+                  <FaStar />
+                  <FaStarHalfAlt />
+                </div>
+                <h3 style={{ color: 'orange' }}>100.000đ</h3>
+                <Button variant="primary" style={MakeCenter}>Thêm vào giỏ hàng</Button>
+              </Card.Body>
+            </Card>
+            <Card style={CardStyle}>
+              <Card.Img variant="top" src={logo} />
+              <hr />
+              <Card.Body>
+                <Card.Title style={{ textAlign: 'center', fontSize: 'xx-large' }}>Sản phẩm A</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up the
+                  bulk of the card's content.
+                </Card.Text>
+                <a href='#home'>Tên người bán</a>
+                <div style={StarStyle}>
+                  <FaStar />
+                  <FaStar />
+                  <FaStar />
+                  <FaStar />
+                  <FaStarHalfAlt />
+                </div>
+                <h3 style={{ color: 'orange' }}>100.000đ</h3>
+                <Button variant="primary" style={MakeCenter}>Thêm vào giỏ hàng</Button>
+              </Card.Body>
+            </Card>
+            <Card style={CardStyle}>
+              <Card.Img variant="top" src={logo} />
+              <hr />
+              <Card.Body>
+                <Card.Title style={{ textAlign: 'center', fontSize: 'xx-large' }}>Sản phẩm A</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up the
+                  bulk of the card's content.
+                </Card.Text>
+                <a href='#home'>Tên người bán</a>
+                <div style={StarStyle}>
+                  <FaStar />
+                  <FaStar />
+                  <FaStar />
+                  <FaStar />
+                  <FaStarHalfAlt />
+                </div>
+                <h3 style={{ color: 'orange' }}>100.000đ</h3>
+                <Button variant="primary" style={MakeCenter}>Thêm vào giỏ hàng</Button>
+              </Card.Body>
+            </Card>
+          </div>
+        </Carousel.Item>
+        <Carousel.Item>
+          <div style={ListItem}>
+            <Card style={CardStyle}>
+              <Card.Img variant="top" src={logo} />
+              <hr />
+              <Card.Body>
+                <Card.Title style={{ textAlign: 'center', fontSize: 'xx-large' }}>Sản phẩm A</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up the
+                  bulk of the card's content.
+                </Card.Text>
+                <a href='#home'>Tên người bán</a>
+                <div style={StarStyle}>
+                  <FaStar />
+                  <FaStar />
+                  <FaStar />
+                  <FaStar />
+                  <FaStarHalfAlt />
+                </div>
+                <h3 style={{ color: 'orange' }}>100.000đ</h3>
+                <Button variant="primary" style={MakeCenter}>Thêm vào giỏ hàng</Button>
+              </Card.Body>
+            </Card>
+            <Card style={CardStyle}>
+              <Card.Img variant="top" src={logo} />
+              <hr />
+              <Card.Body>
+                <Card.Title style={{ textAlign: 'center', fontSize: 'xx-large' }}>Sản phẩm A</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up the
+                  bulk of the card's content.
+                </Card.Text>
+                <a href='#home'>Tên người bán</a>
+                <div style={StarStyle}>
+                  <FaStar />
+                  <FaStar />
+                  <FaStar />
+                  <FaStar />
+                  <FaStarHalfAlt />
+                </div>
+                <h3 style={{ color: 'orange' }}>100.000đ</h3>
+                <Button variant="primary" style={MakeCenter}>Thêm vào giỏ hàng</Button>
+              </Card.Body>
+            </Card>
+            <Card style={CardStyle}>
+              <Card.Img variant="top" src={logo} />
+              <hr />
+              <Card.Body>
+                <Card.Title style={{ textAlign: 'center', fontSize: 'xx-large' }}>Sản phẩm A</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up the
+                  bulk of the card's content.
+                </Card.Text>
+                <a href='#home'>Tên người bán</a>
+                <div style={StarStyle}>
+                  <FaStar />
+                  <FaStar />
+                  <FaStar />
+                  <FaStar />
+                  <FaStarHalfAlt />
+                </div>
+                <h3 style={{ color: 'orange' }}>100.000đ</h3>
+                <Button variant="primary" style={MakeCenter}>Thêm vào giỏ hàng</Button>
+              </Card.Body>
+            </Card>
+          </div>
+        </Carousel.Item>
+      </Carousel>
+      <Button variant='secondary' size='lg' style={MakeCenter}>Xem thêm</Button>
+    </div>
   )
 }
 
