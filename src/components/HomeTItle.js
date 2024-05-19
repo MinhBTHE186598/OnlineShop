09971d5446fcf3d3383840887fc665ce1dfe5acd
DucLAHE182040 <duclahe182040@fpt.edu.vn>
2024-05-19 19:38:46 +0700

    19/5: Them carousel

--- src/components/HomeTItle.js
+++ src/components/HomeTItle.js
@@ -5,17 +5,18 @@ import banner from '../utility/testbanner.jpg'
 const titleStyle = {
   marginTop:'10vh',
   width: '100vw',
-  height: '60vh',
+  height: '40vh',
   textAlign: 'center',
   backgroundImage: `url(${banner})`,
   position: 'relative',
+  boxShadow: '0px 20px 30px'
 }
 const coverStyle = {
   width: '100%',
   height: '100%',
   position: 'absolute',
   backgroundColor: '#000',
-  opacity: '0.75',
+  opacity: '0.6',
 }
 const textStyle = {
   position:'absolute',
@@ -26,7 +27,8 @@ const textStyle = {
   flexDirection: 'column',
   justifyContent: 'center',
   alignItems: 'center',
-  zIndex: '2'
+  zIndex: '2',
+  fontSize:'x-large'
 }
 
 function homeTItle() {
