f0acd77446cbeb9875d00a5e6cec0333f884bddd
Tùng <tungnmhe186473@fpt.edu.vn>
2024-05-20 16:56:44 +0700

    tesing

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
         <Route index element={<HomePage/>}/>
         <Route path="/homeAdmin" element={<HomeAdmin/>}/>
         <Route path="/addSaler" element={<AddSaler/>}/>
+        <Route path="/register" element={<RegisterUser/>}/>
       </Routes>
       </BrowserRouter>
 
--- src/webpages/RegisterUser.js
+++ src/webpages/RegisterUser.js
@@ -2,3 +2,13 @@ import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
+
+function register(){
+    return(
+        <div>
+
+        </div>
+    )
+}
+
+export default register
