31c1dd604557b90d98aca021c0412d739198f466
TrungNDHE182037 <169044473+TrungNDHE182037@users.noreply.github.com>
2024-05-20 11:21:36 +0700

    test

--- src/components/AdminActionList.js
+++ src/components/AdminActionList.js
@@ -3,42 +3,53 @@ import Nav from 'react-bootstrap/Nav';
 import Row from 'react-bootstrap/Row';
 import Tab from 'react-bootstrap/Tab';
 import Badge from 'react-bootstrap/Badge';
+import ListGroup from 'react-bootstrap/ListGroup';
 
 function LeftTabsExample() {
-  return (
-    <div id="wrapper" style={{margin: '125px 30px'}}>
+    const Arr = ['1', '2', '3', '4', '5'];
+    return (
+        <div id="wrapper" style={{ margin: '125px 30px' }}>
 
-    <Tab.Container id="left-tabs-example" defaultActiveKey="first">
-      <Row>
-        <Col sm={3}>
-          <Nav variant="pills" className="flex-column">
-            <Nav.Item>
-              <Nav.Link eventKey="first">Quản lý người dùng</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link eventKey="second">Quản lý sản phẩm</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link eventKey="third">Quản lý banner</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link eventKey="fourth">Kiểm duyệt sản phẩm <Badge bg="warning" pill>14</Badge></Nav.Link>
-            </Nav.Item>
-          </Nav>
-        </Col>
-        <Col sm={9}>
-          <Tab.Content>
-            <Tab.Pane eventKey="first">Quản lý ng dùng ở đây</Tab.Pane>
-            <Tab.Pane eventKey="second">Quản lý sản phẩm ở đây</Tab.Pane>
-            <Tab.Pane eventKey="third">Quản lý banner ở đây</Tab.Pane>
-            <Tab.Pane eventKey="fourth">Kiểm duyệt sản phẩm ở đây</Tab.Pane>
-          </Tab.Content>
-        </Col>
-      </Row>
-    </Tab.Container>
-            
-    </div>
-  );
+            <Tab.Container id="left-tabs-example" defaultActiveKey="first">
+                <Row>
+                    <Col sm={3}>
+                        <Nav variant="pills" className="flex-column">
+                            <Nav.Item>
+                                <Nav.Link eventKey="first">Quản lý người dùng</Nav.Link>
+                            </Nav.Item>
+                            <Nav.Item>
+                                <Nav.Link eventKey="second">Quản lý sản phẩm</Nav.Link>
+                            </Nav.Item>
+                            <Nav.Item>
+                                <Nav.Link eventKey="third">Quản lý banner</Nav.Link>
+                            </Nav.Item>
+                            <Nav.Item>
+                                <Nav.Link eventKey="fourth">Kiểm duyệt sản phẩm <Badge bg="warning" pill>{Arr.length}</Badge></Nav.Link>
+                            </Nav.Item>
+                        </Nav>
+                    </Col>
+                    <Col sm={9}>
+                        <Tab.Content>
+                            <Tab.Pane eventKey="first">Quản lý ng dùng ở đây</Tab.Pane>
+                            <Tab.Pane eventKey="second">Quản lý sản phẩm ở đây</Tab.Pane>
+                            <Tab.Pane eventKey="third">Quản lý banner ở đây</Tab.Pane>
+                            <Tab.Pane eventKey="fourth">
+                                <div>
+                                    {Arr.map((index) => (
+                                        <ListGroup key={index} horizontal='xxl' className="my-2">
+                                            <ListGroup.Item >Dong so {index}</ListGroup.Item>
+                                            <ListGroup.Item onClick='#'>action</ListGroup.Item>
+                                        </ListGroup>
+                                    ))}
+                                </div>
+                            </Tab.Pane>
+                        </Tab.Content>
+                    </Col>
+                </Row>
+            </Tab.Container>
+
+        </div>
+    );
 }
 
 export default LeftTabsExample;
