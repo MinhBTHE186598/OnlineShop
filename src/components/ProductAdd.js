8f81706e20496db76c71df9c2e25ff013a08e6f5
HaiTTHE186621 <haitthe186621@fpt.edu.vn>
2024-05-21 21:41:00 +0700

    update productadd

--- src/components/ProductAdd.js
+++ src/components/ProductAdd.js
@@ -1,39 +1,44 @@
 import Form from 'react-bootstrap/Form';
 import { Button } from 'react-bootstrap';
 import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-
-
 
 function FormGroup() {
     return (
-        <Form>
-            <Form.Group>
-                <Form.Label>Product Name:</Form.Label>
-                <Form.Control type="productName" placeholder="Enter product name" />
-            </Form.Group>
-            <Form.Group>
-                <Form.Label>Product Price:</Form.Label>
-                <Form.Control type="productPrice" placeholder="Enter product price" />
-            </Form.Group>
-            <Form.Group>
-                <Form.Label>Product Picture:</Form.Label>
-                <Form.Control type="productPicture" placeholder="Enter product picture url" />
-                <input id="filebutton" name="filebutton" class="input-file" type="file"/>
-            </Form.Group>
-            <Form.Group>
-                <Form.Label>Stock Quantity:</Form.Label>
-                <Form.Control type="productQuantity" placeholder="Enter amount of this product in stock" />
-            </Form.Group>
-            <Form.Group>
-                <Form.Label>Product Description:</Form.Label>
-                <Form.Control type="productDescription" placeholder="Enter product description" />
-            </Form.Group>
-            <Button variant="primary" type="submit">
-                Submit
-            </Button>
-        </Form>
+        <Container style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+            <div style={{ backgroundColor: 'white', width: '100%', maxWidth: '600px', padding: '20px', borderRadius: '8px', boxShadow: '0 0 10px rgba(0, 0 ,0, 0.1)' }}>
+                <Form>
+                    <h2 style={{ textAlign: 'center' }}>Add an Product</h2>
+                    <Form.Group>
+                        <Form.Label>Product Name:</Form.Label>
+                        <Form.Control type="text" placeholder="Enter product name" />
+                    </Form.Group>
+                    <Form.Group>
+                        <Form.Label>Product Price:</Form.Label>
+                        <Form.Control type="text" placeholder="Enter product price" />
+                    </Form.Group>
+                    <Form.Group>
+                        <Form.Label>Product Picture:</Form.Label><br />
+                        <input id="filebutton" name="filebutton" className="input-file" type="file" />
+                    </Form.Group>
+                    <Form.Group>
+                        <Form.Label>Stock Quantity:</Form.Label>
+                        <Form.Control type="text" placeholder="Enter amount of this product in stock" />
+                    </Form.Group>
+                    <Form.Group>
+                        <Form.Label>Product Description:</Form.Label>
+                        <Form.Control type="text" placeholder="Enter product description" />
+                    </Form.Group>
+                    <div style={{ display: 'flex', justifyContent: 'center', marginTop: '20px', gap: '10px' }}>
+                        <Button variant="danger" onClick={() => window.location.href = "/"}>
+                            Cancel
+                        </Button>
+                        <Button variant="primary" type="submit">
+                            Submit
+                        </Button>
+                    </div>
+                </Form>
+            </div>
+        </Container>
     );
 }
 
