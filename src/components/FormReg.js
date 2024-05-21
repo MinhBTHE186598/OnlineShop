66c42f80c3b37bab475af8420a155b8c3e0365b9
MinhBTHE186598 <minhbthe186598@fpt.edu.vn>
2024-05-21 20:29:31 +0700

    sửa formreg

--- src/components/FormReg.js
+++ src/components/FormReg.js
@@ -7,14 +7,12 @@ function FormGroupExample() {
   return (
     <Container fluid="sm" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
       <Form style={{ width: '300px', textAlign: 'center' }}>
-        <h2>Create SHOP</h2>
-        <p>Enter your shop name</p>
+        <h2>Đăng kí bán hàng</h2>
+        <p>Làm chủ cuộc chơi của bạn</p>
         <Form.Group className="mb-3" controlId="formGroupNameShop">
           <Form.Control type="text" placeholder="Tên shop" />
         </Form.Group>
-        <Form.Group className="mb-3" controlId="formGroupAddress">
-          <Form.Control type="text" placeholder="Địa chỉ lấy hàng" />
-        </Form.Group>
+    
         <Form.Group className="mb-3" controlId="formGroupEmail">
           <Form.Control type="email" placeholder="Email" />
         </Form.Group>
@@ -22,7 +20,7 @@ function FormGroupExample() {
           <Form.Control type="text" placeholder="Phone number" />
         </Form.Group>
         <Button variant="dark" type="submit" style={{ width: '100%' }}>
-          Sign up
+          Đăng kí
         </Button>
         <p style={{ marginTop: '10px', fontSize: 'small' }}>
           By clicking continue, you agree to our <a href="#">Terms of Service</a> and <a href="#">Privacy Policy</a>
