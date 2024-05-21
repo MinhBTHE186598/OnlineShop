081b1a44e8525dca453e5401050f794557c2e11e
Tùng <tungnmhe186473@fpt.edu.vn>
2024-05-21 21:34:08 +0700

    vietsub

--- src/components/FormRegister.js
+++ src/components/FormRegister.js
@@ -7,14 +7,14 @@ function RegisterBoard() {
   return (
     <Container fluid="sm" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
       <Form style={{ width: '300px', textAlign: 'center' }}>
-        <h2>Register</h2>
+        <h2>Đăng kí tài khoản</h2>
         
         <Form.Group className="mb-3" controlId="formGroupNameShop">
-          <Form.Control type="text" placeholder="Username" />
+          <Form.Control type="text" placeholder="Tài khoản" />
           <Form.Control type="gmail" placeholder="Gmail" />
-          <Form.Control type="text" placeholder="Phone number" />
-          <Form.Control type="password" placeholder="Password" />
-          <Form.Control type="password" placeholder="Confirm your password" />
+          <Form.Control type="text" placeholder="Số điện thoại" />
+          <Form.Control type="password" placeholder="Mật khẩu" />
+          <Form.Control type="password" placeholder="Xác nhận mật khẩu" />
 
         </Form.Group>
         <Button variant="dark" type="submit" style={{ width: '100%' }}>
