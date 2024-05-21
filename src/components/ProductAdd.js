a4b57b590c07eb640e07790048990fc789c80b74
HaiTTHE186621 <haitthe186621@fpt.edu.vn>
2024-05-21 21:54:33 +0700

    tiep tuc update add product

--- src/components/ProductAdd.js
+++ src/components/ProductAdd.js
@@ -7,33 +7,34 @@ function FormGroup() {
         <Container style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
             <div style={{ backgroundColor: 'white', width: '100%', maxWidth: '600px', padding: '20px', borderRadius: '8px', boxShadow: '0 0 10px rgba(0, 0 ,0, 0.1)' }}>
                 <Form>
-                    <h2 style={{ textAlign: 'center' }}>Add an Product</h2>
+                    <h2 style={{ textAlign: 'center' }}>Đăng ký sản phẩm mới cho cửa hàng</h2>
+                    <hr/>
                     <Form.Group>
-                        <Form.Label>Product Name:</Form.Label>
-                        <Form.Control type="text" placeholder="Enter product name" />
+                        <Form.Label><b>Tên sản phẩm:</b></Form.Label>
+                        <Form.Control type="text" placeholder="Nhập tên sản phẩm" />
                     </Form.Group>
                     <Form.Group>
-                        <Form.Label>Product Price:</Form.Label>
-                        <Form.Control type="text" placeholder="Enter product price" />
+                        <Form.Label><b>Giá sản phẩm:</b></Form.Label>
+                        <Form.Control type="text" placeholder="Nhập giá sản phẩm" />
                     </Form.Group>
                     <Form.Group>
-                        <Form.Label>Product Picture:</Form.Label><br />
+                        <Form.Label><b>Hình ảnh sản phẩm:</b></Form.Label><br />
                         <input id="filebutton" name="filebutton" className="input-file" type="file" />
                     </Form.Group>
                     <Form.Group>
-                        <Form.Label>Stock Quantity:</Form.Label>
-                        <Form.Control type="text" placeholder="Enter amount of this product in stock" />
+                        <Form.Label><b>Số lượng trong kho:</b></Form.Label>
+                        <Form.Control type="text" placeholder="Nhập số lượng sản phẩm còn lại trong kho" />
                     </Form.Group>
                     <Form.Group>
-                        <Form.Label>Product Description:</Form.Label>
-                        <Form.Control type="text" placeholder="Enter product description" />
+                        <Form.Label><b>Mô tả sản phẩm:</b></Form.Label>
+                        <Form.Control as="textarea" rows={5} placeholder="Nhập mô tả sản phẩm" />
                     </Form.Group>
                     <div style={{ display: 'flex', justifyContent: 'center', marginTop: '20px', gap: '10px' }}>
                         <Button variant="danger" onClick={() => window.location.href = "/"}>
-                            Cancel
+                            Huỷ
                         </Button>
                         <Button variant="primary" type="submit">
-                            Submit
+                            Đăng bán
                         </Button>
                     </div>
                 </Form>
