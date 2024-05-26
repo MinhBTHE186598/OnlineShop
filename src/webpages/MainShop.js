4b63b8187c3f5dc4cfaad09dcbb083f56a77dc29
duclahe182040 <duclahe182040@fpt.edu.vn>
2024-05-26 22:44:57 +0700

    Thêm title cho MainShop

--- src/webpages/MainShop.js
+++ src/webpages/MainShop.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
+import MainShopTitle from '../components/MainShopTitle'
 import MainShopItemList from '../components/MainShopItemList'
 
 function MainShop() {
     return (
         <div>
             <Header />
+            <MainShopTitle/>
             <MainShopItemList />
             <Footer />
         </div>
