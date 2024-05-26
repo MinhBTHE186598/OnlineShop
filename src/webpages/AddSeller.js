7306561e4f6d8c081a0c68f1ba3337abdef69aff
duclahe182040 <duclahe182040@fpt.edu.vn>
2024-05-26 21:35:39 +0700

    sửa lại bug

--- src/webpages/AddSeller.js
+++ src/webpages/AddSeller.js
@@ -2,7 +2,7 @@ import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
-import FormReg from '../components/FormAddSaler'
+import FormReg from '../components/FormAddSeller'
 
 function AddSeller() {
     return (
