dda2b29eed856c980286bdd0dbad7892a8965c66
MinhBTHE186598 <minhbthe186598@fpt.edu.vn>
2024-05-26 22:56:59 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import './App.css';
+
 import HomePage from './webpages/Home';
 import HomeAdmin from './webpages/HomeAdmin';
-import { BrowserRouter,Routes,Route } from 'react-router-dom'; 
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import RegisterUser from './webpages/RegisterUser';
 import AboutUs from './webpages/AboutUs';
 import AddSeller from './webpages/AddSeller';
+import MainShop from './webpages/MainShop';
 
 function App() {
   return (
     <div>
       {/* npm install react-router-dom neu bi loi */}
       <BrowserRouter>
+
       <Routes>
         <Route index element={<HomePage/>}/>
         <Route path="/homeAdmin" element={<HomeAdmin/>}/>
         <Route path="/addSeller" element={<AddSeller/>}/>
         <Route path="/register" element={<RegisterUser/>}/>
-        <Route path="/about-us" element={<AboutUs/>}/>
+        <Route path="/aboutUs" element={<AboutUs/>}/>
+        <Route path="/mainShop" element={<MainShop/>}/>
       </Routes>
+
       </BrowserRouter>
 
     </div>
