b87c8219f2e2535f14d2714526dd865e9eb4903a
TrungNDHE182037 <169044473+TrungNDHE182037@users.noreply.github.com>
2024-05-27 12:55:38 +0700

    xoa thu muc dist

--- src/backend-onlineshop/dist/controllers/user.js
+++
@@ -1,4 +0,0 @@
-const getUser = (req, res) => {
-  return res.send('USER CONTROLLER');
-};
-module.exports = getUser;
--- src/backend-onlineshop/dist/routes/user.js
+++
@@ -1,4 +0,0 @@
-const user = require('../controllers/user');
-const router = require('express').Router();
-router.get('/', user.getUser);
-module.exports = router;
