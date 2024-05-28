17eac63fe31a8b2c6cc667534fb86792bea4daa4
DucLAHE182040 <duclahe182040@fpt.edu.vn>
2024-05-28 21:40:01 +0700

    lay du lieu Banner thanh cong

--- src/backend-onlineshop/src/routes/index.js
+++ src/backend-onlineshop/src/routes/index.js
@@ -1,8 +1,10 @@
 const user = require("./user")
+const banner = require("./banner")
 
 const initRoutes = (app) => {
 
     app.use('/user', user)
+    app.use('/banner', banner)
     
     return app.use("/", (req, res) => {
         return res.send("hello w2")
--- src/components/HomeCarousel.js
+++ src/components/HomeCarousel.js
@@ -1,45 +1,44 @@
 import React from 'react'
 import Carousel from 'react-bootstrap/Carousel';
-import picture1 from '../utility/carousel1.jpg';
-import picture2 from '../utility/carousel2.jpg';
-import picture3 from '../utility/carousel3.jpg';
+import { useState, useEffect } from 'react';
+
+
 
 const imageStyle = {
   objectFit: 'cover',
-  objectPosition: '50% 60%',
+  objectPosition: '50% 50%',
   height: '70vh'
 }
 
-function homeCarousel() {
+function HomeCarousel() {
+  const [bannerList, setBannerList] = useState([{}])
+
+  useEffect(() => {
+    fetch("/banner/get").then(
+      response => response.json()
+    ).then(
+      data => {
+        setBannerList(data)
+      }
+    )
+  }, [])
+
+
   return (
-    <div style={{ padding: '50px 0', width:'90vw', margin:'auto' }}>
+    <div style={{ padding: '50px 0', width: '90vw', margin: 'auto' }}>
       <Carousel>
-        <Carousel.Item>
-          <img className="d-block w-100" src={picture2} alt="First slide" style={imageStyle}/>
-          <Carousel.Caption>
-            <h5>First slide label</h5>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img className="d-block w-100" src={picture1} alt="Second slide" style={imageStyle}/>
-          <Carousel.Caption>
-            <h5>Second slide label</h5>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img className="d-block w-100" src={picture3} alt="Third slide" style={imageStyle}/>
-          <Carousel.Caption>
-            <h5>Third slide label</h5>
-            <p>
-              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-            </p>
-          </Carousel.Caption>
-        </Carousel.Item>
+        {bannerList.map((banner, index) => (
+          <Carousel.Item key={index}>
+            <img className="d-block w-100" src={banner.BannerPic} alt={banner.BannerID} style={imageStyle} />
+            <Carousel.Caption>
+              <h5>Title</h5>
+              <p>Caption</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+        ))}
       </Carousel>
     </div>
   )
 }
 
-export default homeCarousel
+export default HomeCarousel
