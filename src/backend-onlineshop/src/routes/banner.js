de4981055922382cd47868ca2ec67ab38d9b8de1
DucLAHE182040 <duclahe182040@fpt.edu.vn>
2024-05-28 16:38:57 +0700

    update DAO

--- src/backend-onlineshop/src/routes/banner.js
+++ src/backend-onlineshop/src/routes/banner.js
@@ -2,6 +2,9 @@ const banner = require('../controllers/banner')
 const router = require('express').Router();
 
 router.get('/get', banner.getBanner)
+router.get('/delete/:id', banner.deleteBanner)
+router.get('/add', banner.addBanner)
+router.get('/edit/:id', banner.editBanner)
 
 
 module.exports = router
