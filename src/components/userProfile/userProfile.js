bbc9fba1505d2262cf13503efdd201ddb379dbd5
HaiTTHE186621 <haitthe186621@fpt.edu.vn>
2024-06-06 14:55:02 +0700

    Xong frontend User Profile

--- src/components/userProfile/userProfile.js
+++ src/components/userProfile/userProfile.js
@@ -11,14 +11,15 @@ import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../../utility/register.css';
+import { Table } from 'react-bootstrap';
 
 function Profile() {
     const borderColor = '#0d6efd';
 
     return (
-        <Container fluid style={{ backgroundImage: `url(${bgi})`, height: '100vh', backgroundSize: 'cover' }}>
+        <Container fluid style={{ backgroundImage: `url(${bgi})`, backgroundSize: 'cover', minHeight: '900px' }}>
             <Row>
-                <Col md={3} style={{ backgroundColor: '#f8f9fa', display: 'flex', flexDirection: 'column', alignItems: 'center', height: '100vh', boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)', marginLeft: '8%' }}>
+                <Col md={3} style={{ backgroundColor: '#f8f9fa', display: 'flex', flexDirection: 'column', alignItems: 'center', boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)', marginLeft: '8%', minHeight: '900px' }}>
                     <div style={{
                         width: '200px',
                         height: '200px',
@@ -41,8 +42,10 @@ function Profile() {
                     </div>
                     <div>
                         <p>ID: 12345</p>
+
                     </div>
-                    <div style={{display:'flex',flexDirection:'column',justifyContent:'center',alignItems:'center'}}>
+                    <hr style={{ color: 'red', width: '300px' }} />
+                    <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
                         <Button style={{
                             width: '200px',
                             height: '50px',
@@ -83,17 +86,69 @@ function Profile() {
                     </div>
                 </Col>
 
-                <Col md={7} style={{ backgroundColor: '#f8f9fa', height: '100vh', boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)', marginLeft: '10px' }}>
+                <Col md={7} style={{ backgroundColor: '#f8f9fa', boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)', marginLeft: '10px' }}>
                     <div style={{ marginTop: '15vh', marginLeft: '5vh' }}>
-                        <h1>My Profile</h1>
-                        <p>Update your profile for improved security</p>
+                        <h1><b>HỒ SƠ CỦA TÔI</b></h1>
+                        <p>Hãy cập nhật hồ sơ để tăng bảo mật cho tài khoản của bạn</p>
                     </div>
-                    <hr style={{color:'red'}}/>
-                    <Col style={{ marginLeft: '10vw', marginTop: '5vh' }}>
-                        <Row>
-                            <h5>Username</h5>
-                        </Row>
-                    </Col>
+                    <hr style={{ color: 'red' }} />
+                    <Table style={{
+                        marginLeft: '3vw',
+                        marginTop: '5vh',
+                        border: '',
+                        maxWidth: '50vw'
+                    }}>
+                        <tbody>
+                            <tr style={{ borderTop: '1px solid black', borderBottom: '1px solid black', height: '50px' }}>
+                                <td></td>
+                                <td style={{ fontSize: '25px', width: '400px' }}><b>Phương thức liên lạc</b></td>
+                                <td ></td>
+                            </tr>
+                            <tr>
+                                <td style={{ fontWeight: 'bold', textAlign: 'end', width: '10vw' }}>Tên đăng nhập</td>
+                                <td>user1</td>
+                                <td></td>
+                            </tr>
+                            <tr style={{ borderBottom: '1px solid black' }}>
+                                <td style={{ fontWeight: 'bold', textAlign: 'end', width: '10vw' }}>Mật khẩu</td>
+                                <td>***********</td>
+                                <td style={{ textAlign: 'end' }}><a href="#">Đổi mật khẩu</a></td>
+                            </tr>
+                            <tr style={{ borderBottom: '1px solid black', height: '50px' }}>
+                                <td></td>
+                                <td style={{ fontSize: '25px' }}><b>Phương thức liên lạc</b></td>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <td style={{ fontWeight: 'bold', textAlign: 'end', width: '10vw' }}>Địa chỉ nhận hàng</td>
+                                <td>text holder smth smth smth text holder smth smth smth text holder smth smth smth text holder smth smth smth text holder smth smth smth text holder smth smth smth text holder smth smth smth text holder smth smth smth text holder smth smth smth text holder </td>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <td style={{ fontWeight: 'bold', textAlign: 'end', width: '10vw' }}>Email</td>
+                                <td>text holder smth smth smth</td>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <td style={{ fontWeight: 'bold', textAlign: 'end', width: '10vw' }}>Số điện thoại</td>
+                                <td >0123456789</td>
+                                <td></td>
+                            </tr>
+                        </tbody>
+                    </Table>
+                    <Button style={{ position: 'absolute', marginBottom: '10vh', marginLeft: '10vw', width: '40vw', height: '50px', backgroundColor: 'white', color: `${borderColor}` }}
+                        onMouseEnter={(e) => {
+                            e.target.style.backgroundColor = `${borderColor}`;
+                            e.target.style.color = 'white'
+                        }}
+
+                        onMouseLeave={(e) => {
+                            e.target.style.backgroundColor = 'white';
+                            e.target.style.color = `${borderColor}`
+                        }}
+                    >
+                        Bạn đang gặp vấn đề? Liên hệ với chúng tôi ngay!
+                    </Button>
                 </Col>
             </Row>
         </Container>
