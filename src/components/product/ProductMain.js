6df03f5b03fa2a9623faa20b7766742ee9a94b18
DucLAHE182040 <duclahe182040@fpt.edu.vn>
2024-06-13 16:33:57 +0700

    Xong frontend viewProduct

--- src/components/product/ProductMain.js
+++ src/components/product/ProductMain.js
@@ -14,7 +14,6 @@ const containerStyle = {
     height: 'max-content',
     width: '90vw',
     margin: '10vh auto',
-    padding: '20px',
     overflow: 'auto'
 }
 
@@ -84,26 +83,29 @@ function ProductMain(props) {
                             </Carousel.Item>
                         </Carousel>
                     </Col>
-                    <Col>
-                        <h1>{product.ProductName}</h1>
-                        <p>{product.ProductDescription}</p>
-                        <p>Danh mục: {categories.find(category => category.CategoryID === product.CategoryID).CategoryName}</p>
-                        <a href={`/profile/${sellerList.find(seller => seller.SellerID === product.SellerID).UserID}`}>Người bán: {sellerList.find(seller => seller.SellerID === product.SellerID).SellerName}</a>
-                        <p>Số lượng đăng bán: {product.ProductQuantity}</p>
-                        <p style={{ color: product.ProductQuantity === 0 ? 'red' : 'green' }}>Tình trạng: {product.ProductQuantity === 0 ? 'Hết hàng' : 'Còn hàng'}</p>
-                        <div style={StarStyle}>
-                            {Array(stars.find(star => star.ProductID === product.ProductID)?.ProductStar || 0).fill(<FaStar />)}
-                            {Array(5 - (stars.find(star => star.ProductID === product.ProductID)?.ProductStar || 0)).fill(<FaRegStar />)}
+                    <Col style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center' }}>
+                        <div>
+                            <h1 style={{ marginBottom: '50px' }}>{product.ProductName}</h1>
+                            <h3>Mô tả sản phẩm: </h3>
+                            <h4 style={{ marginBottom: '30px', backgroundColor: '#ddd', padding: '15px', borderRadius: '20px', border: 'solid 1px black', width: '80%' }}>{product.ProductDescription}</h4>
+                            <p style={{ marginBottom: '30px', fontSize: 'x-large' }}>Danh mục: {categories.find(category => category.CategoryID === product.CategoryID).CategoryName}</p>
+                            <a href={`/profile/${sellerList.find(seller => seller.SellerID === product.SellerID).UserID}`} style={{ fontSize: 'x-large' }}>Người bán: {sellerList.find(seller => seller.SellerID === product.SellerID).SellerName}</a>
+                            <p style={{ marginBottom: '0', fontSize: 'larger' }}>Số lượng đăng bán: {product.ProductQuantity}</p>
+                            <p style={{ color: product.ProductQuantity === 0 ? 'red' : 'green', fontSize: 'larger', marginBottom: '30px' }}>Tình trạng: {product.ProductQuantity === 0 ? 'Hết hàng' : 'Còn hàng'}</p>
+                            <div style={StarStyle}>
+                                {Array(stars.find(star => star.ProductID === product.ProductID)?.ProductStar || 0).fill(<FaStar />)}
+                                {Array(5 - (stars.find(star => star.ProductID === product.ProductID)?.ProductStar || 0)).fill(<FaRegStar />)}
+                            </div>
+                            <p style={{ fontSize: 'xx-large', color: 'orange', margin: '0' }}>{product.ProductPrice}đ</p>
+                            <Button variant="primary" size='lg'>
+                                Thêm vào giỏ hảng
+                            </Button>
                         </div>
-                        <p>{product.ProductPrice}</p>
-                        <Button variant="primary" size='lg'>
-                            Thêm vào giỏ hảng
-                        </Button>
                     </Col>
                 </Row>
                 <Row style={{ padding: '100px 0' }}>
                     <Col xs={8} style={reviewStyle}>
-                        <h3 style={{ textAlign: 'center' }}>Đánh giá sản phẩm</h3>
+                        <h3 style={{ textAlign: 'center', marginBottom: '30px' }}>Đánh giá sản phẩm</h3>
                         <ProductReview id={product.ProductID} />
                         {isLogin ?
                             <Form>
