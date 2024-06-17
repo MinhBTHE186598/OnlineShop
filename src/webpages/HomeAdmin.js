72a1995db7403286fe9ded3a15c73887561d4af1
TrungNDHE182037 <169044473+TrungNDHE182037@users.noreply.github.com>
2024-06-17 11:19:26 +0700

    them dieu huong den trang manager cho admin o header

--- src/webpages/HomeAdmin.js
+++ src/webpages/HomeAdmin.js
@@ -2,11 +2,10 @@ import Header from '../components/common/Header'
 import AdminAction from '../components/homeAdmin/AdminActionList'
 import Footer from '../components/common/Footer'
 import { useUser } from '../components/context/UserContext'
-import { useNavigate,Navigate } from "react-router-dom";
+import {Navigate } from "react-router-dom";
 
 function HomeAdmin() {
-    const navigate = useNavigate();
-    const { user, setUser, userRole, setUserRole, isLogin, setIsLogin } = useUser();
+    const {  userRole, isLogin} = useUser();
     if(isLogin&&userRole==='Admin'){
     return (
         <div>
