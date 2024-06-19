12ae66f4422da5ec813f8baa45455b1d84f5c4d6
DucLAHE182040 <duclahe182040@fpt.edu.vn>
2024-06-19 23:57:29 +0700

    fix login

--- src/backend-onlineshop/src/routes/user.js
+++ src/backend-onlineshop/src/routes/user.js
@@ -2,7 +2,7 @@ const user = require('../controllers/user')
 const router = require('express').Router();
 
 router.delete('/delete/:id', user.deleteUser)
-router.get('/getUser', user.getUser)
+router.get('/get', user.getUser)
 router.post('/registerUser', user.registerUser)
 router.get('/getinf/:id', user.getUserByID)
 router.put('/update', user.updateUser)
