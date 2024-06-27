3ab18a32c3d5eb2b9ff5904347f18618728cc377
TungNMHE186473 <tungnmhe186473@fpt.edu.vn>
2024-06-27 14:56:25 +0700

    add useEffect for check login

--- src/components/contactUs/contactUs.js
+++ src/components/contactUs/contactUs.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
@@ -7,25 +7,30 @@ import Row from 'react-bootstrap/Row';
 import { useUser } from '../context/UserContext';
 import { useNavigate } from 'react-router-dom';
 import '../../utility/contact.css';
+
 const ContactForm = () => {
   const [title, setTitle] = useState('');
   const [content, setContent] = useState('');
   const { user, isLogin } = useUser();
-  const Navigate = useNavigate();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!isLogin) {
+      alert('Please login first!');
+      navigate('/login');
+    }
+  }, [isLogin, navigate]);
+
   const handleSubmit = async (event) => {
     event.preventDefault();
     const UserID = user.UserID;
-    if (!isLogin) {
-      alert('Please login first!');
-      Navigate('/login');
-      return;
-    }
     
     try {
-      const response = await axios.post('http://localhost:5000/contact/addContact',
-         {title, 
-          content,
-          UserID});
+      const response = await axios.post('http://localhost:5000/contact/addContact', {
+        title, 
+        content,
+        UserID
+      });
       console.log(response.data);
       alert('Form submitted successfully!');
     } catch (error) {
@@ -36,11 +41,10 @@ const ContactForm = () => {
   return (
     <Container fluid className="form-container" style={{backgroundColor: '#0d6efd'}}>
       <Row className="form-wrapper">
-        <Form onSubmit={handleSubmit} >
+        <Form onSubmit={handleSubmit}>
           <div>
-            <h2 style={{textAlign: 'center'}}> Liên hệ</h2>
+            <h2 style={{textAlign: 'center'}}>Liên hệ</h2>
             <label>Tiêu đề</label>
-            
             <input 
               type="text" 
               placeholder='Tiêu đề'
@@ -49,8 +53,9 @@ const ContactForm = () => {
             />
           </div>
           <div>
-            <label >Nội dung</label>
-            <textarea id='content'
+            <label>Nội dung</label>
+            <textarea
+              id='content'
               value={content} 
               placeholder='Bạn hãy miêu tả vấn đề gặp phải'
               onChange={(e) => setContent(e.target.value)} 
