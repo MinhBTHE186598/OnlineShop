4a1738bb72aaff1b491ca45d4220532258e99630
TungNMHE186473 <tungnmhe186473@fpt.edu.vn>
2024-07-02 20:41:58 +0700

    list Products better

--- src/components/homeShipper/BillDetailModal.js
+++ src/components/homeShipper/BillDetailModal.js
@@ -5,11 +5,11 @@ import PropTypes from 'prop-types';
 
 function BillDetailModal({ show, onHide, billDetails }) {
   return (
-    <Modal show={show} onHide={onHide}>
+    <Modal show={show} onHide={onHide} size="lg">
       <Modal.Header closeButton>
         <Modal.Title>Bill Details</Modal.Title>
       </Modal.Header>
-      <Modal.Body>
+      <Modal.Body style={{ maxHeight: 'calc(100vh - 210px)', overflowY: 'auto' }}>
         {billDetails.map((billDetail, index) => (
           <div key={index}>
             <p><strong>Product ID:</strong> {billDetail.ProductID}</p>
