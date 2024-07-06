37c13274e64e5b16c1f045d0c525f95462938e69
HaiTTHE186621 <haitthe186621@fpt.edu.vn>
2024-07-06 22:19:27 +0700

    remove commented lines + delete button

--- src/components/sellerShop/sellerShopManager.js
+++ src/components/sellerShop/sellerShopManager.js
@@ -4,20 +4,16 @@ import { Container } from "react-bootstrap";
 import { useUser } from '../context/UserContext';
 import { useState, useEffect } from "react";
 import React from "react";
-// import axios from 'axios';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import Image from 'react-bootstrap/Image';
 import Nav from 'react-bootstrap/Nav';
 import Tab from 'react-bootstrap/Tab';
 import SellerProduct from './sellerProductList';
-// import { useParams } from 'react-router-dom'
 import bgi from '../../utility/background_1.jpg';
 import EditProfileModal from "./EditShopProfileModal";
 
 function SellerShop() {
-    // const Arr = ['1', '2', '3', '4', '5'];
-    // const { id } = useParams();
 
     const { user } = useUser();
 
@@ -26,20 +22,11 @@ function SellerShop() {
     const handleCloseEdit = () => setShowEdit(false);
 
     const [profileInf, setProfileInf] = useState({});
-    // const [userList, setUserList] = React.useState([]);
 
     const editProfile = (profile) => {
         setProfileInf(profile);
         setShowEdit(true);
     }
-
-    // React.useEffect(() => {
-    //     fetch("http://localhost:5000/user/get")
-    //         .then(response => response.json())
-    //         .then(data => {
-    //             setUserList(data)
-    //         })
-    // }, [])
 
     useEffect(() => {
         fetch("http://localhost:5000/seller/get")
@@ -50,21 +37,6 @@ function SellerShop() {
     }, []);
 
     const myshop = sellerList.find(seller => seller.UserID === user.UserID);
-
-    // const [managerInf, setManagerInf] = React.useState({});
-    // useEffect(() => {
-    //     if (myshop)
-    //         setManagerInf(sellManagerList.find(manager => manager.UserID === myshop.UserID));
-    // }, [myshop]);
-
-    // React.useEffect(() => {
-    //     fetch("http://localhost:5000/sellManager/getInf")
-    //         .then(response => response.json())
-    //         .then(data => {
-    //             setSellManagerList(data)
-    //         })
-    // }, [])
-
 
     return (
         myshop ? (
@@ -98,8 +70,7 @@ function SellerShop() {
                         <h3>ID Người Quản Lý : {myshop.SellManagerID}</h3>
                     </Col>
                     <Col md={2} style={{ margin: '25px' }}>
-                    <Button variant="primary" onClick={() => { editProfile(myshop) }} style={{ marginTop: '10px', marginLeft: '10px', width: '100%', height: '75px' }}>Sửa Thông Tin Cửa Hàng</Button>
-                    <Button variant="primary" style={{ marginTop: '10px', marginLeft: '10px', width: '100%', height: '75px', backgroundColor: 'red' }}>TÔI MUỐN XOÁ CỬA HÀNG</Button>
+                    <Button onClick={() => { editProfile(myshop) }} style={{ marginTop: '10px', marginLeft: '10px', width: '100%', height: '75px' }}>Sửa Thông Tin Cửa Hàng</Button>
                     </Col>
                 </Row>
                 <Row style={{ display: 'flex', backgroundColor: 'white', width: '90vw', height: 'max-content', minHeight: '20vh', margin: '5vh auto', borderRadius: '10px', boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)' }}>
