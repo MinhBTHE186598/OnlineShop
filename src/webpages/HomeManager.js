b4e4dbe7ef9a5edea7bf21c02c36c6abae663a89
MinhBTHE186598 <minhbthe186598@fpt.edu.vn>
2024-07-11 11:57:07 +0700

    navigate cho manager

--- src/webpages/HomeManager.js
+++ src/webpages/HomeManager.js
@@ -2,7 +2,12 @@ import React from 'react'
 import Header from '../components/common/Header'
 import ManagerAction from '../components/homeManager/ManagerAction'
 import Footer from '../components/common/Footer'
+import { useUser } from '../components/context/UserContext'
+import {Navigate } from "react-router-dom";
+
 function HomeManager() {
+  const { userRole,isLogin } = useUser();
+  if(isLogin&&userRole==='sellManager'){
   return (
     <div>
         <Header/>
@@ -10,6 +15,10 @@ function HomeManager() {
         <Footer/>
     </div>
   )
+}else{
+  return(
+      <Navigate to="/"/>
+  )
 }
-
+}
 export default HomeManager
